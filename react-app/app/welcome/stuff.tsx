333a9682d012fb61a801a862c4ef7c8bb4c622c1
karlhdenby <karlhdenby@gmail.com>
2026-07-26 16:27:35 -0700

    made into react project

--- react-app/app/welcome/stuff.tsx
+++ react-app/app/welcome/stuff.tsx
@@ -30,13 +30,13 @@ const layerMapper = {
 const App = () => {
   const result = fakeData.map((e) => (
     <div
-      key={${e.shapeType}-${e.shapePosition.x}-${e.shapePosition.y}}
+      key={`${e.shapeType}-${e.shapePosition.x}-${e.shapePosition.y}`}
       style={{
         display: "flex",
         position: "absolute",
         top: e.shapePosition.y,
         left: e.shapePosition.x,
-        backgroundColor: rgba(${e.shapeColor.r}, ${e.shapeColor.g}, ${e.shapeColor.b}, ${e.shapeColor.a}),
+        backgroundColor: (`rgba(${e.shapeColor.r}, ${e.shapeColor.g}, ${e.shapeColor.b}, ${e.shapeColor.a})`),
         width: e.shapeLocation.width,
         height: e.shapeLocation.height,
       }}
